70d1ff63c741d2b4db3be5f4924857a7c61cf39a
Kieran Metcalfe <kieran@ascent-creative.co.uk>
2023-10-31 19:14:50 +0000

    updates to helper method

--- src/helpers.php
+++ src/helpers.php
@@ -1,5 +1,5 @@
 <?php
 
-function UserSetting($name) {
-    return AscentCreative\UserSettings\Models\Setting::get($name);
+function UserSetting($name, $context=null, $user=null) {
+    return AscentCreative\UserSettings\Models\Setting::get(name: $name, context: $context, user_id: $user);
 }
